eff3acb5a2a1809d920b812a4fb0b9b5488309be
fais edathil <faiesedathil@gmail.com>
2022-09-02 20:15:53 +0300

    set up cover

---
+++ src/components/HeroBanner/HeroBanner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './HeroBanner.css'
+function HeroBanner() {
+  return (
+    <div className='banner'>
+        <img className='cover-image' 
+        src='https://wallpapercave.com/dwp1x/wp11390760.jpg'/>
+    <h1>Thallumala Fan Page</h1>
+    </div>
+  )
+}
+
+export default HeroBanner
