77866653ce1ed3e8668c3b710ad975c293886170
fais edathil <faiesedathil@gmail.com>
2022-09-03 17:31:14 +0300

    animate hero banner

--- src/components/HeroBanner/HeroBanner.jsx
+++ src/components/HeroBanner/HeroBanner.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import './HeroBanner.css'
 import profile from '../../assets/images/profile.jpg'
+import {motion} from 'framer-motion'
 function HeroBanner() {
   return (
     <div className='banner'>
-        <img className='cover-image'  src={profile}/>
-    <h1>Thallumala Fan Page</h1>
+        <motion.img 
+        className='cover-image'  
+        src={profile}
+        initial={{x:"-100vw"}}
+        animate={{x:0}}
+        transition={{delay:1,type:'tween',duration:1}}
+        />
+    <motion.h1 
+    animate={{y:0}}
+    initial={{y:"100vh"}}
+    transition={{delay:1}}
+    >Thallumala Fan Page
+    </motion.h1>
     </div>
   )
 }
