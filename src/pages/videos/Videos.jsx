e7de14b5c0ccd3ea095d727cf48a6a7cebc6dde1
fais edathil <faiesedathil@gmail.com>
2022-09-04 21:23:48 +0300

    populate video page

--- src/pages/videos/Videos.jsx
+++ src/pages/videos/Videos.jsx
@@ -3,13 +3,19 @@ import pic from "../../assets/images/youtube.jpg";
 import "./videos.css";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
+import YouTube from "react-youtube";
 function Videos() {
-  useEffect(()=>{
-  window.scrollTo(0,0)
-  },[])
-  const navigate =useNavigate()
+ 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  const navigate = useNavigate();
+  const opts={
+    height:'200px',
+    width:'300px'
+  }
   return (
-    <div >
+    <div>
       <div className="videos">
         <motion.h1
           initial={{ x: "100vw" }}
@@ -17,65 +23,60 @@ function Videos() {
           transition={{ delay: 0.5 }}
           className="title"
         >
-          Official Videos
+          Thallumala Videos
         </motion.h1>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 1, duration: 2 }}
-          className="trailer"
+        <div
+        className='main-trailer'
         >
-          <img src={pic} />
-        </motion.div>
+         <YouTube 
+         videoId="s_OdRGbpKUA" 
+         />
+        </div>
       </div>
       <div className="allTrailers">
         <div className="trailerBox">
-          <img src={pic} />
+          <YouTube opts={opts} videoId="Ljhpp2m1T34" />
         </div>
 
         <div className="trailerBox">
-          <img src={pic} />
+        <YouTube opts={opts} videoId="KKc5ttd88qc" />
         </div>
 
         <div className="trailerBox">
-          <img src={pic} />
+        <YouTube opts={opts} videoId="48cK_5NQcGw" />
         </div>
       </div>
-      <h2 className="fan-title">Fan Made Videos</h2>
+      <h2 className="fan-title">Interviews</h2>
       <div className="fan-videos">
         <div className="trailerBox">
-          <img src={pic} />
+        <YouTube opts={opts} videoId="PPcZGXJPqWc" />
         </div>
 
         <div className="trailerBox">
-          <img src={pic} />
+        <YouTube opts={opts} videoId="BKtm2VyRJDQ" />
+        </div>
+        <div className="trailerBox">
+        <YouTube opts={opts} videoId="iKRi0o5f8i0" />
         </div>
 
-        <div className="trailerBox">
-          <img src={pic} />
-        </div>
       </div>
-
+      <h2 className="fan-title">Trolls and Reels</h2>
       <div className="fan-videos">
         <div className="trailerBox">
-          <img src={pic} />
+        <YouTube opts={opts} videoId="I7wVYSG6PYo" />
         </div>
 
         <div className="trailerBox">
-          <img src={pic} />
+        <YouTube opts={opts} videoId="nvs-h1cr9DQ" />
         </div>
 
         <div className="trailerBox">
-          <img src={pic} />
+        <YouTube opts={opts} videoId="fS99GRvLyiU" />
         </div>
       </div>
       <div className="btn">
-      <button 
-      onClick={()=>navigate('/')}>
-        Back to home page
-        </button>
+        <button onClick={() => navigate("/")}>Back to home page</button>
       </div>
-    
     </div>
   );
 }
